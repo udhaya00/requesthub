37b4c658c85693675d6f39ac1873912de696938d
udhaya00 <buv369@gmail.com>
2026-04-17 12:12:57 +0530

    fix terraform backend

--- infra/aws/backend.tf
+++ infra/aws/backend.tf
@@ -15,6 +15,12 @@ resource "aws_ecr_repository" "backend" {
   }
 }
 
+backend "s3" {
+  bucket = "smart-request-hub-prod-361103952894-b89a47"
+  key    = "terraform.tfstate"
+  region = "ap-south-1"
+}
+
 resource "aws_ecr_lifecycle_policy" "backend" {
   repository = aws_ecr_repository.backend.name
 
